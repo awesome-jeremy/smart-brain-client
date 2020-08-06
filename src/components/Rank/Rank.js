79eae88cb4abfa298c834569528edf7a681dcb71
Ray <ray644302280@gmail.com>
2020-08-06 11:32:14 +1000

    Get rank emoji from AWS lambda and render on the page

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emoji_rank: ''
+    }
+  }
+
+  componentDidMount() {
+    const { entries } = this.props;
+    fetch(`https://cecietuom8.execute-api.ap-southeast-2.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.emoji_rank) {
+          this.setState({ emoji_rank: data.emoji_rank })
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className='white f3'>
+          Rank Badge: {this.state.emoji_rank}
+        </div>
+
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+
+    )
+  }
 }
 
 export default Rank;
